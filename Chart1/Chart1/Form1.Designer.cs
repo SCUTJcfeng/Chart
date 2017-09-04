3502e5b99b8139faaaf7290517f5c7f94a1b3032
Jc Feng <jcfeng2013@gmail.com>
2017-09-05 07:58:09 +0800

    Add files via upload

---
+++ Chart1/Chart1/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Chart1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title2 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.cht1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.cht1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cht1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.cht1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.cht1.Legends.Add(legend2);
+            this.cht1.Location = new System.Drawing.Point(35, 100);
+            this.cht1.Name = "cht1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.cht1.Series.Add(series2);
+            this.cht1.Size = new System.Drawing.Size(813, 345);
+            this.cht1.TabIndex = 0;
+            this.cht1.Text = "chart1";
+            title2.Name = "rfdgfdgfd ";
+            this.cht1.Titles.Add(title2);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(882, 493);
+            this.Controls.Add(this.cht1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.cht1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart cht1;
+    }
+}
+
---
+++ Chart1/Chart1/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Chart1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] X = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12" };
+            int[] Y1 = { 210, 220, 198, 187, 201, 206, 234, 198, 204, 210, 211, 192 };
+            int[] Y3 = { 200, 220, 198, 187, 251, 206, 234, 198, 204, 210, 201, 192 };
+            int[] Y2 = new int[12];
+            for (int i = 0; i < Y2.Length; i++) Y2[i] = 200;
+
+
+            cht1.Series.Add(new Series("Series2"));
+
+            cht1.Series[0].Points.DataBindXY(X, Y1);
+            cht1.Series[1].Points.DataBindY(Y2);
+            //cht1.Series[1].Points.DataBindY(Y3);
+
+
+
+            //X
+            cht1.ChartAreas[0].AxisX.Interval = 1;
+            cht1.ChartAreas[0].AxisX.Title = "时间";
+            cht1.ChartAreas[0].AxisX.MajorGrid.Enabled = false;
+
+            //Y
+            cht1.ChartAreas[0].AxisY.Title = "产能";
+            cht1.ChartAreas[0].AxisY.TextOrientation = System.Windows.Forms.DataVisualization.Charting.TextOrientation.Horizontal;
+            cht1.ChartAreas[0].AxisY.MajorGrid.Enabled = false;
+
+            //Show Value
+            cht1.Series[0].Label = "#VAL";
+            //cht1.Series[1].Label = "#VAL";
+
+            //图例
+            cht1.Series[0].LegendText = "实际产能";
+            cht1.Series[1].LegendText = "目标产能";
+
+            //折线
+            cht1.Series[1].ChartType = SeriesChartType.Line;
+
+            //颜色
+            cht1.Series[1].Color = Color.Red;
+        }
+    }
+}
